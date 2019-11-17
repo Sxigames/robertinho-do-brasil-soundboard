8286fcc37344c13045b4349c4081be4231c6e1cb
Sxigames <sxigames63@gmail.com>
2019-11-17 19:25:51 -0300

    Revert "Conserta PWA cache update"

--- js/index.js
+++ js/index.js
@@ -1,38 +1,12 @@
-let newWorker;
-  function showUpdateBar() {
-    let snackbar = document.getElementById('snackbar');
-    snackbar.className = 'show';
-  }
-  // The click event on the pop up notification
-  document.getElementById('reload').addEventListener('click', function(){
-    newWorker.postMessage({ action: 'skipWaiting' });
-  });
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js').then(reg => {
-      reg.addEventListener('updatefound', () => {
-        // A wild service worker has appeared in reg.installing!
-        newWorker = reg.installing;
-        newWorker.addEventListener('statechange', () => {
-          // Has network.state changed?
-          switch (newWorker.state) {
-            case 'installed':
-              if (navigator.serviceWorker.controller) {
-                // new update available
-                showUpdateBar();
-              }
-              // No update available
-              break;
-          }
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+        .then(function () {
+            console.log('service worker registered');
+        })
+        .catch(function () {
+            console.warn('service worker failed');
         });
-      });
-    });
-    let refreshing;
-    navigator.serviceWorker.addEventListener('controllerchange', function () {
-      if (refreshing) return;
-      window.location.reload();
-      refreshing = true;
-    });
-  }
+}
 
 $(document).ready(function() {
     lowLag.init({'urlPrefix':'./audio/'});
